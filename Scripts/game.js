cb5a0c2e2fcd664481ab384da3d68495ad0489df
jagpreetjattana <jagpreet@hotmail.com>
2016-02-04 18:36:50 -0500

    internal document

--- Scripts/game.js
+++ Scripts/game.js
@@ -28,6 +28,7 @@ var control;
 var gui;
 var stats;
 var humanoid = new object3d;
+//these are the cubes used to make the cubeman
 var cube1;
 var cube2;
 var cube3;
@@ -50,108 +51,98 @@ function init() {
     cube = new Mesh(cubeGeometry, cubeMaterial);
     cube.castShadow = true;
     cube.position.x = 2;
-    // cube.position.y=0.5;
     cube.position.y = -10;
     cube.position.z = 3;
-    //scene.add(cube);
     humanoid.add(cube);
-    /*  cubeGeometry = new CubeGeometry(1, 1, 2);
-      cubeMaterial = new LambertMaterial({color:0x00ff00, opacity:0.5});
-      cube = new Mesh(cubeGeometry, cubeMaterial);
-      cube.castShadow = true;
-      cube.position.x=4;
-      cube.position.y=0.5;
-      cube.position.z=3;
-      //scene.add(cube);
-      humanoid.add(cube);*/
+    //Add a Cube to make the foot of humanoid
     cubeGeometry = new CubeGeometry(1, 1, 2);
     cube1 = new Mesh(cubeGeometry, cubeMaterial);
     cube1.castShadow = true;
     cube1.position.x = 2;
-    //cube1.position.y=0.5;
     cube1.position.y = -10;
     cube1.position.z = -3;
     humanoid.add(cube1);
+    //Add a Cube to make the leg of humanoid
     cubeGeometry = new CubeGeometry(2, 6, 2);
     cube2 = new Mesh(cubeGeometry, cubeMaterial);
     cube2.castShadow = true;
     cube2.position.x = 1;
-    //cube2.position.y=3;
     cube2.position.y = -7.5;
     cube2.position.z = -3;
     humanoid.add(cube2);
+    //Add a Cube to make the leg of humanoid
     cubeGeometry = new CubeGeometry(2, 6, 2);
     cube3 = new Mesh(cubeGeometry, cubeMaterial);
     cube3.castShadow = true;
     cube3.position.x = 1;
-    //cube3.position.y=3;
     cube3.position.y = -7.5;
     cube3.position.z = 3;
     humanoid.add(cube3);
+    //Add a Cube to make the stomach of humanoid
     cubeGeometry = new CubeGeometry(4, 10, 10);
     cube4 = new Mesh(cubeGeometry, cubeMaterial);
     cube4.castShadow = true;
     cube4.position.x = 1;
-    //cube4.position.y=11;
     cube4.position.y = 0.5;
     cube4.position.z = 0;
     humanoid.add(cube4);
+    //Add a Cube to make the neck of humanoid
     cubeGeometry = new CubeGeometry(2, 2, 2);
     cube5 = new Mesh(cubeGeometry, cubeMaterial);
     cube5.castShadow = true;
     cube5.position.x = 1;
-    //cube5.position.y=17;
     cube5.position.y = 6.5;
     cube5.position.z = 0;
     humanoid.add(cube5);
+    //Add a Cube to make the face of humanoid
     cubeGeometry = new CubeGeometry(4, 4, 4);
     cube6 = new Mesh(cubeGeometry, cubeMaterial);
     cube6.castShadow = true;
     cube6.position.x = 1;
-    //cube6.position.y=19;
     cube6.position.y = 8.5;
     cube6.position.z = 0;
     humanoid.add(cube6);
+    //Add a Cube to make the arm of humanoid
     cubeGeometry = new CubeGeometry(2, 2, 6);
     cube7 = new Mesh(cubeGeometry, cubeMaterial);
     cube7.castShadow = true;
     cube7.position.x = 1;
-    //cube7.position.y=14;
     cube7.position.y = 3.5;
     cube7.position.z = 8;
     humanoid.add(cube7);
+    //Add a Cube to make the arm of humanoid
     cubeGeometry = new CubeGeometry(2, 2, 6);
     cube8 = new Mesh(cubeGeometry, cubeMaterial);
     cube8.castShadow = true;
     cube8.position.x = 1;
-    //cube8.position.y=14;
     cube8.position.y = 3.5;
     cube8.position.z = -8;
     humanoid.add(cube8);
+    //Add a Cube to make the eye of humanoid
     cubeGeometry = new CubeGeometry(2, .5, 1);
     cube9 = new Mesh(cubeGeometry, cubeMaterial);
     cube9.castShadow = true;
     cube9.position.x = 2.2;
-    //cube9.position.y=20;
     cube9.position.y = 9.5;
     cube9.position.z = 1;
     humanoid.add(cube9);
+    //Add a Cube to make the eye of humanoid
     cubeGeometry = new CubeGeometry(2, .5, 1);
     cube10 = new Mesh(cubeGeometry, cubeMaterial);
     cube10.castShadow = true;
     cube10.position.x = 2.2;
-    //cube10.position.y=20;
     cube10.position.y = 9.5;
     cube10.position.z = -1;
     humanoid.add(cube10);
+    //Add a Cube to make the mouth of humanoid    
     cubeGeometry = new CubeGeometry(2, .5, 1.5);
     cube11 = new Mesh(cubeGeometry, cubeMaterial);
     cube11.castShadow = true;
     cube11.position.x = 2;
-    //cube11.position.y=18;
     cube11.position.y = 7.5;
     cube11.position.z = 0;
     humanoid.add(cube11);
+    //adding humanoid to scene
     scene.add(humanoid);
     //Add a Plane to the Scene
     planeGeometry = new PlaneGeometry(60, 60);
@@ -159,23 +150,18 @@ function init() {
     plane = new Mesh(planeGeometry, planeMaterial);
     plane.receiveShadow = true;
     plane.rotation.x = -0.5 * Math.PI;
-    //plane.position.y = -2;
     plane.position.y = -12.5;
     scene.add(plane);
     console.log("Added Plane Primative to scene...");
     // Add a SpotLight to the scene
     spotLight = new SpotLight(0xffffff);
     spotLight.position.set(21, 70, 19);
-    //spotLight.position.set (35, 35, -30);
-    //spotLight.rotation.set(-0.8, 42.7, 19.5);
     spotLight.lookAt(new Vector3(0, 0, 0));
-    //spotLight.lookAt(plane.position);
     spotLight.castShadow = true;
     scene.add(spotLight);
     console.log("Added Spot Light to Scene");
     // add extras
     gui = new GUI();
-    //control = new Control(0.005, 0.005,0.005, cubeMaterial.opacity, cubeMaterial.color.getHex());
     control = new Control(0.005, 0.005, 0.005, cubeMaterial.opacity, cubeMaterial.color.getHex());
     addControl(control);
     addStatsObject();
@@ -208,7 +194,6 @@ function gameLoop() {
     humanoid.rotation.y += control.YrotationSpeed;
     humanoid.rotation.x += control.XrotationSpeed;
     humanoid.rotation.z += control.ZrotationSpeed;
-    // humanoid.rotation.z+=control.rotationSpeedz;
     renderer.render(scene, camera);
 }
 // Setup default renderer
